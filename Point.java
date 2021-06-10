d3b75843fb372b2f4c057defe02e1d004475d252
hannahw1004 <hannah.wee.2004@gmail.com>
2021-06-09 19:27:09 -0700

    updated Point Class

--- Point.java
+++ Point.java
@@ -5,54 +5,75 @@ public class Point extends Main{
 
     public int playerNum; 
     public int dealerNum; 
+
+    public int betAmount = 0;
     
     public void comparePlayerAndDealer(DrawingPanel dp) {
         PlayerDecks playerD = new PlayerDecks(); 
         DealerDecks dealerD = new DealerDecks();
         
         playerNum = playerD.PlayerSumCard(); 
-        dealerNum = dealerD.dealerSumCard(); 
+        dealerNum = dealerD.dealerSumCard();
 
-        if (playerNum == 21) {
-            win(); 
-        } else if (dealerNum < playerNum && playerNum < 21) {
-            win(); 
-        } else if (playerNum < 21 && 21 < dealerNum) {
-            win(); 
-        } else if (playerNum < dealerNum && dealerNum < 21) {
-            lose(); 
-        } else if (dealerNum < 21 && 21 < playerNum) {
-            lose(); 
-        } else if (playerNum == dealerNum) {
-            draw(); 
+
+        if(playerNum == 21){
+            win();
+        }else if (playerNum > 21) {
+            lose();
+        }else if(playerNum < 21){
+            if(playerNum > dealerNum){
+                win();
+            }else if(playerNum < dealerNum){
+                lose();
+            }else if(dealerNum == 21){
+                lose();
+            }else if(dealerNum > 21){
+                win();
+            }else if(dealerNum == playerNum){
+                draw();
+            }
         }
+    }
+    public void win() {
+        playerPoint =+ betAmount;
+    }
+
+    public void draw() {
+        playerPoint =+ (betAmount/2);
+        dealerPoint =+ (betAmount/2);
 
     }
 
-    public void win() { }
-
-    public void draw() { }
-
-    public void lose() { }
+    public void lose() {
+        dealerPoint =+ betAmount;
+    }
 
     public void is10ChipClicked() {
-
+        dealerPoint =- 10;
+        playerPoint =- 10;
+        betAmount =+ 20;
     }
 
     public void is20ChipClicked() {
-        
+        dealerPoint =- 20;
+        playerPoint =- 20;
+        betAmount =+ 40;
     }
 
     public void is50ChipClicked() {
-        
+        dealerPoint =- 50;
+        playerPoint =- 50;
+        betAmount =+ 100;
     }
 
     public void is100ChipClicked() {
-        
+        dealerPoint =- 100;
+        playerPoint =- 100;
+        betAmount =+ 200;
     }
+}  
 
-}
-
+    
 
 
 
